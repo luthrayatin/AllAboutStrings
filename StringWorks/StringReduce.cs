b0ec99e0906423420ec3ee9d7ebcb1348cb2d4e3
Yatin Luthra <luthrayatin@gmail.com>
2017-03-08 13:48:52 +1000

    Modified project and added class library for handling multiple functionalities.

---
+++ StringWorks/StringReduce.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringWorks
+{
+    public class StringReduce
+    {
+        public string Go(string str)
+        {
+            StringBuilder sb = null;
+            var stack = new Stack<char>();
+            foreach (var t in str)
+            {
+                if (stack.Count > 0 && stack.Peek() == t)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(t);
+                }
+            }
+            sb = new StringBuilder();
+            if (stack.Count > 0)
+            {
+                while (stack.Count > 0)
+                {
+                    sb.Insert(0, stack.Pop());
+                }
+            }
+            else
+            {
+                return "Empty String";
+            }
+            return sb.ToString();
+        }
+    }
+}
--- StringsConsoleApp/Program.cs
+++ StringsConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringWorks;
 
 namespace StringsConsoleApp
 {
@@ -10,37 +11,8 @@ namespace StringsConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ReduceString(Console.ReadLine()));
-        }
-
-        static string ReduceString(string str)
-        {
-            StringBuilder sb = null;
-            var stack = new Stack<char>();
-            foreach (var t in str)
-            {
-                if (stack.Count > 0 && stack.Peek() == t)
-                {
-                    stack.Pop();
-                }
-                else
-                {
-                    stack.Push(t);
-                }
-            }
-            sb = new StringBuilder();
-            if (stack.Count > 0)
-            {
-                while (stack.Count > 0)
-                {
-                    sb.Insert(0, stack.Pop());
-                }
-            }
-            else
-            {
-                return "Empty String";
-            }
-            return sb.ToString();
+            var obj = new StringReduce();
+            Console.WriteLine(obj.Go(Console.ReadLine()));
         }
     }
 }
