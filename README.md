7a11e9056d0173ed7d0eabf201fb94fbc6e85c73
Yatin Luthra <luthrayatin@gmail.com>
2017-03-08 13:35:20 +1000

    Added ReduceString function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AllAboutStrings
 Working with Strings
+
+1. ReduceString function
+Input: string
+Returns: string
+
+All adjacent pairs of same letters will be removed, so, "aabcc" will become "b", "aaabccc" will become "abc" and "baab" will become an empty string in which case it will return "Empty String".
--- StringsConsoleApp/Program.cs
+++ StringsConsoleApp/Program.cs
@@ -10,6 +10,37 @@ namespace StringsConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ReduceString(Console.ReadLine()));
+        }
+
+        static string ReduceString(string str)
+        {
+            StringBuilder sb = null;
+            var stack = new Stack<char>();
+            foreach (var t in str)
+            {
+                if (stack.Count > 0 && stack.Peek() == t)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(t);
+                }
+            }
+            sb = new StringBuilder();
+            if (stack.Count > 0)
+            {
+                while (stack.Count > 0)
+                {
+                    sb.Insert(0, stack.Pop());
+                }
+            }
+            else
+            {
+                return "Empty String";
+            }
+            return sb.ToString();
         }
     }
 }
